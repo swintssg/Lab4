9a2406cabea77f056935e7813db96cdd4a601576
swintssg <swintssg@gmail.com>
2017-09-27 17:46:34 -0400

    My code asks for you name and welcomes you to my java program. Enjoy!

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.println("Hello, _______");
+        System.out.println("Please enter your name: ");
+        name = sc.nextLine();
+
+        System.out.println("Welcome, " + name + " to my small java program!");
+        System.out.println("I hope you enjoyed yourself!");
+    }
+}
